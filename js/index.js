0c90e9fa4efab5ce448a6a7ea04e7febafb6eb47
0hangyeol0 <145204468+0hangyeol0@users.noreply.github.com>
2024-06-28 15:35:42 +0900

    .

--- js/index.js
+++ js/index.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
   })
   console.log($('.project_pan'))
 
-
   // project의 화살표를 클릭할 시 다음 프로젝트 페이지로 넘기기
   $(document).on('click', '.next_page', function() {
     $('.project_pan').fadeOut();
@@ -21,17 +20,39 @@ $(document).ready(function() {
     sec_bang++;
   })
 
-
-  function slide() {
-
-  }
-
-
   $(document).on('click', '#project_page', function() {
     $('.sec').fadeOut();
     $('#project').fadeIn();
   })
 
+
+  $(document).on('click','.item', function(){
+    let modal = 
+    `<div class="modal">
+        <div class="modal_item_popup">
+            <button class="modal_close">X</button>
+
+            <div class="modal_banner">
+                <div class="modal_banner_box">
+                    <div class="modal_banner_title">
+                    </div>
+                    <div class="play_btn_box">
+                        <div class="play_btn">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="ltr-4z3qvp e1svuwfo1" data-name="Play" aria-labelledby=":R19kpt9llkm:" aria-hidden="true"><path d="M5 2.69127C5 1.93067 5.81547 1.44851 6.48192 1.81506L23.4069 11.1238C24.0977 11.5037 24.0977 12.4963 23.4069 12.8762L6.48192 22.1849C5.81546 22.5515 5 22.0693 5 21.3087V2.69127Z" fill="currentColor"></path></svg>
+                        </div>
+                        <div class="play_txt">재생</div> 
+                    </div>
+                </div>
+            </div>
+            
+            <div class="modal_desc">
+            </div>
+        </div>
+    </div>
+    `
+    
+    $('body').append(modal);
+  });
 
 
 })
