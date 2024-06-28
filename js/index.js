63da5e94c264600566b8a2ba404d18e1b010ae49
0hangyeol0 <145204468+0hangyeol0@users.noreply.github.com>
2024-06-28 15:27:43 +0900

    .

--- js/index.js
+++ js/index.js
@@ -1,21 +1,37 @@
-
-
 $(document).ready(function() {
-  console.log("시작");  
+  console.log("시작")
+  let sec_bang = 0;
+  let timer = 1000;
 
   // 보고싶은 sec과 연결된 li들을 클릭했을때 나머지 sec들을 none 처리하고 클릭한 sec은 flex주기
   $(document).on('click', '.nav_li', function(){
+    let sec_index = $(this).index();
+    console.log("sec의 인덱스 번호를 nav에 비례해 사용하고 있다: " + sec_index);
+    $('.sec').fadeOut();
+    $('.sec').eq(sec_index).fadeIn();
+  })
+  console.log($('.project_pan'))
 
-      let sec_index = $(this).index();
-      $('.sec > .sec_desc').css({ opacity: 0 })
-        
-      $('.sec > .sec_desc').eq(sec_index).css({ opacity: 1});
+
+  // project의 화살표를 클릭할 시 다음 프로젝트 페이지로 넘기기
+  $(document).on('click', '.next_page', function() {
+    $('.project_pan').fadeOut();
+    $('.project_pan').eq(sec_bang % 2).fadeIn();
+
+    sec_bang++;
+  })
+
+
+  function slide() {
+
+  }
+
+
+  $(document).on('click', '#project_page', function() {
+    $('.sec').fadeOut();
+    $('#project').fadeIn();
   })
 
 
 
-
-
-
-  
 })
